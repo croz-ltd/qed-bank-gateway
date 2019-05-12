455ca3d821aea45447051593ba324fc09d33eab3
Milan Jovanovic <mjovanovic@croz.net>
2019-05-13 00:44:50 +0200

    Added CORS records and Currency and Country infos in Balance model

--- src/main/java/net/croz/qed/bank/gateway/controller/GatewayController.java
+++ src/main/java/net/croz/qed/bank/gateway/controller/GatewayController.java
@@ -7,6 +7,7 @@ import net.croz.qed.bank.gateway.service.BalanceGateway;
 import net.croz.qed.bank.gateway.service.TransactionGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class GatewayController {
 
